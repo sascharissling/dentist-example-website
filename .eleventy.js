4ec42a2e4d315d8770bf796f55cae8dcaf45a6f2
Sascha Rissling <sascha.rissling@gmail.com>
2024-10-29 15:11:33 +0100

    fix deploy

--- .eleventy.js
+++ .eleventy.js
@@ -13,124 +13,116 @@ const configServer = require("./src/config/server");
 const filterPostDate = require("./src/config/postDate");
 const isProduction = configServer.isProduction;
 
-
 module.exports = function (eleventyConfig) {
-    /**=====================================================================
+  /**=====================================================================
           EXTENSIONS - Recognising non-default languages as templates 
     =======================================================================*/
-    /** https://www.11ty.dev/docs/languages/custom/ */
+  /** https://www.11ty.dev/docs/languages/custom/ */
 
-    /**
-     *  CSS EXTENSION
-     *  Setting up CSS files to be recognised as aN eleventy template language. This allows our minifier to read CSS files and minify them
-     */
-    eleventyConfig.addTemplateFormats("css");
-    eleventyConfig.addExtension("css", configCss);
+  /**
+   *  CSS EXTENSION
+   *  Setting up CSS files to be recognised as aN eleventy template language. This allows our minifier to read CSS files and minify them
+   */
+  eleventyConfig.addTemplateFormats("css");
+  eleventyConfig.addExtension("css", configCss);
 
-    /**
-     *  JS EXTENSION
-     *  Sets up JS files as an eleventy template language, which are compiled by esbuild. Allows bundling and minification of JS
-     */
-    eleventyConfig.addTemplateFormats("js");
-    eleventyConfig.addExtension("js", configJs);
-    /**=====================================================================
+  /**
+   *  JS EXTENSION
+   *  Sets up JS files as an eleventy template language, which are compiled by esbuild. Allows bundling and minification of JS
+   */
+  eleventyConfig.addTemplateFormats("js");
+  eleventyConfig.addExtension("js", configJs);
+  /**=====================================================================
                                 END EXTENSIONS
     =======================================================================*/
 
-
-    /**=====================================================================
+  /**=====================================================================
                   PLUGINS - Adds additional eleventy functionality 
     =======================================================================*/
-    /** https://www.11ty.dev/docs/plugins/ */
+  /** https://www.11ty.dev/docs/plugins/ */
 
-    /**
-     *  ELEVENTY NAVIGATION
-     *  Sets up the eleventy navigation plugin for a scalable navigation as used in _includes/components/header.html
-     *  https://github.com/11ty/eleventy-navigation
-     */
-    eleventyConfig.addPlugin(pluginEleventyNavigation);
+  /**
+   *  ELEVENTY NAVIGATION
+   *  Sets up the eleventy navigation plugin for a scalable navigation as used in _includes/components/header.html
+   *  https://github.com/11ty/eleventy-navigation
+   */
+  eleventyConfig.addPlugin(pluginEleventyNavigation);
 
-    /**
-     *  AUTOMATIC SITEMAP GENERATION 
-     *  Automatically generate a sitemap, using the domain in _data/client.json
-     *  https://www.npmjs.com/package/@quasibit/eleventy-plugin-sitemap
-     */
-    eleventyConfig.addPlugin(pluginSitemap, configSitemap);
+  /**
+   *  AUTOMATIC SITEMAP GENERATION
+   *  Automatically generate a sitemap, using the domain in _data/client.json
+   *  https://www.npmjs.com/package/@quasibit/eleventy-plugin-sitemap
+   */
+  eleventyConfig.addPlugin(pluginSitemap, configSitemap);
 
-    /**
-     *  MINIFIER 
-     *  When in production ("npm run build" is ran), minify all HTML, CSS, JSON, XML, XSL and webmanifest files.
-     *  https://github.com/benjaminrancourt/eleventy-plugin-files-minifier
-     */
-    if (isProduction) {
-        eleventyConfig.addPlugin(pluginMinifier);
-    }
-    /**=====================================================================
+  /**
+   *  MINIFIER
+   *  When in production ("npm run build" is ran), minify all HTML, CSS, JSON, XML, XSL and webmanifest files.
+   *  https://github.com/benjaminrancourt/eleventy-plugin-files-minifier
+   */
+  if (isProduction) {
+    eleventyConfig.addPlugin(pluginMinifier);
+  }
+  /**=====================================================================
                                 END PLUGINS
     =======================================================================*/
 
-
-    /**======================================================================
+  /**======================================================================
        PASSTHROUGHS - Copy source files to /public with no 11ty processing
     ========================================================================*/
-    /** https://www.11ty.dev/docs/copy/ */
+  /** https://www.11ty.dev/docs/copy/ */
 
-    eleventyConfig.addPassthroughCopy("./src/assets", {
-        filter: [
-            "**/*",
-            "!**/*.js"
-        ]
-    });
-    eleventyConfig.addPassthroughCopy("./src/admin");
-    eleventyConfig.addPassthroughCopy("./src/_redirects");
-    /**=====================================================================
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/_redirects");
+  /**=====================================================================
                               END PASSTHROUGHS
     =======================================================================*/
 
-    /**======================================================================
+  /**======================================================================
                FILTERS - Modify data in template files at build time
     ========================================================================*/
-    /** https://www.11ty.dev/docs/filters/ */
+  /** https://www.11ty.dev/docs/filters/ */
 
-    /**
-     *  Converts dates from JSDate format (Fri Dec 02 18:00:00 GMT-0600) to a locale format.
-     *  Use - {{ "DATE GOES HERE" | postDate }}
-     *  https://moment.github.io/luxon/api-docs/index.html#datetime
-     */
-    eleventyConfig.addFilter("postDate", filterPostDate);
-    /**=====================================================================
+  /**
+   *  Converts dates from JSDate format (Fri Dec 02 18:00:00 GMT-0600) to a locale format.
+   *  Use - {{ "DATE GOES HERE" | postDate }}
+   *  https://moment.github.io/luxon/api-docs/index.html#datetime
+   */
+  eleventyConfig.addFilter("postDate", filterPostDate);
+  /**=====================================================================
                                     END FILTERS
     =======================================================================*/
 
-    /**======================================================================
+  /**======================================================================
                   SHORTCODES - Output data using JS at build time
     ========================================================================*/
-    /** https://www.11ty.dev/docs/shortcodes/ */
+  /** https://www.11ty.dev/docs/shortcodes/ */
 
-    /**
-     *  Gets the current year, which can be outputted with {% year %}. Used for the footer copyright. Updates with every build.
-     *  Use - {% year %}
-     */
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-    /**=====================================================================
+  /**
+   *  Gets the current year, which can be outputted with {% year %}. Used for the footer copyright. Updates with every build.
+   *  Use - {% year %}
+   */
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  /**=====================================================================
                                 END SHORTCODES
     =======================================================================*/
 
-    /**=====================================================================
+  /**=====================================================================
                                 SERVER SETTINGS
     =======================================================================*/
-    eleventyConfig.setServerOptions(configServer);
-    /**=====================================================================
+  eleventyConfig.setServerOptions(configServer);
+  /**=====================================================================
                               END SERVER SETTINGS
     =======================================================================*/
 
-    return {
-        dir: {
-            input: "src",
-            output: "public",
-            includes: "_includes",
-            data: "_data",
-        },
-        htmlTemplateEngine: "njk",
-    };
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes",
+      data: "_data",
+    },
+    htmlTemplateEngine: "njk",
+  };
 };
